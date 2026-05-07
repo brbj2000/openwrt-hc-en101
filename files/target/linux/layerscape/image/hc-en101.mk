9fa12be6b9fd0e7a3ca646dfc5c69314c1b7f570
brbj2000 <m15112812515@163.com>
2026-05-07 23:43:03 +0800

    Update hc-en101.mk

--- files/target/linux/layerscape/image/hc-en101.mk
+++ files/target/linux/layerscape/image/hc-en101.mk
@@ -1,33 +1,35 @@
-# Add HC-EN101 device definition to OpenWrt layerscape image Makefile
-# This patch should be applied to: target/linux/layerscape/image/armv8_64b.mk
-# 
-# Place this as a standalone file; the build workflow will merge it.
+# HC-EN101 device definition for OpenWrt layerscape image Makefile
+#
+# Boot flow (from U-Boot printenv):
+#   QSPI NOR: RCW(64KB@0x0) + U-Boot(640KB@0x100000)
+#   eMMC:     p1(ext4,fitImage) + p2(squashfs,rootfs)
+#   U-Boot:   ext4load mmc 0:1 0xa0000000 fitImage; bootm 0xa0000000
+#
+# This device uses eMMC as the primary boot storage for kernel+rootfs.
+# The QSPI NOR flash only stores bootloader (RCW + U-Boot) and is NOT
+# reprogrammed by the OpenWrt image.
+#
+# Inherits from Device/fsl-sdboot template which provides:
+#   - KERNEL = kernel-bin | gzip | fit gzip (fitImage with DTB embedded)
+#   - IMAGES = sdcard.img.gz sysupgrade.bin
+#   - IMAGE/sysupgrade.bin = sysupgrade-tar | append-metadata
 
 define Device/hctele_hc-en101
+  $(Device/fsl-sdboot)
   DEVICE_VENDOR := HuaichenTelecom
   DEVICE_MODEL := HC-EN101
   DEVICE_ALT_MODEL := TE1104H0
   DEVICE_DTS := fsl-ls1012a-hc-en101
   DEVICE_DTS_DIR := $(DTS_DIR)/freescale
-  DEVICE_PACKAGES := layerscape-ppfe kmod-ppfe \
-    kmod-gpio-button-hotplug kmod-leds-gpio
-  BLOCKSIZE := 256KiB
-  IMAGE_SIZE := 64m
-  IMAGES := firmware.bin sysupgrade.bin
-  KERNEL := kernel-bin | gzip | fit gzip $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
-  KERNEL_INITRAMFS := kernel-bin | gzip | fit gzip $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
-  IMAGE/firmware.bin := \
+  DEVICE_PACKAGES := \
+    layerscape-ppfe \
+    kmod-ppfe \
+    kmod-gpio-button-hotplug \
+    kmod-leds-gpio
+  IMAGE/sdcard.img.gz := \
     ls-clean | \
-    ls-append $(1)-bl2.pbl | pad-to 1M | \
-    ls-append $(1)-fip.bin | pad-to 5M | \
-    ls-append $(1)-uboot-env.bin | pad-to 10M | \
-    ls-append pfe.itb | pad-to 15M | \
-    ls-append-dtb $$(DEVICE_DTS) | pad-to 16M | \
-    append-kernel | pad-to $$(BLOCKSIZE) | \
-    append-rootfs | pad-rootfs | check-size
-  IMAGE/sysupgrade.bin := \
-    append-kernel | pad-to $$(BLOCKSIZE) | \
-    append-rootfs | pad-rootfs | \
-    check-size $(LS_SYSUPGRADE_IMAGE_SIZE) | append-metadata
+    ls-append-sdhead $(1) | pad-to 16M | \
+    ls-append-kernel | pad-to $(LS_SD_ROOTFSPART_OFFSET)M | \
+    append-rootfs | pad-to $(LS_SD_IMAGE_SIZE)M | gzip
 endef
 TARGET_DEVICES += hctele_hc-en101
